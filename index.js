df081a46ede3e8aeeee2d8f16bb1bac5f5685c41
Md Farhan Islam Shuvro <shuvro.cadet@gmail.com>
2023-06-09 23:58:40 +0600

    modified

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log("hello");
 console.log("It's really good!");
 
-window.alert("shuvro er page");
-let username= window.prompt("what's your name?");
-console.log(username);
+//window.alert("shuvro er page");
+//let username= window.prompt("what's your name?");
+//console.log(username);
 
 
 let myname = "shuvro";
@@ -19,4 +19,35 @@ document.getElementById("mybutton").onclick = function(){
     document.getElementById("mylebel").innerHTML = "Hello " + inputname;
     
 
+}
+
+let count=0;
+document.getElementById("decreasebutton").onclick = function(){
+    count-=1;
+    document.getElementById("countlabel").innerHTML = count;
+}
+document.getElementById("increasebutton").onclick = function(){
+    count+=1;
+    document.getElementById("countlabel").innerHTML = count;
+}
+document.getElementById("resetbutton").onclick = function(){
+    count=0;
+    document.getElementById("countlabel").innerHTML = count;
+}
+
+
+
+document.getElementById("rbutton").onclick = function(){
+    
+    var x;
+    var y;
+    var z;
+    x=document.getElementById("fnumber").value;
+    y=document.getElementById("lnumber").value;
+    console.log(x);
+    console.log(y);
+    z=Math.floor(Math.random()*(y-x+1)+Math.round(x-0.5));
+    console.log(z);
+
+    document.getElementById("pr").innerHTML = "Your random number is : " + z;
 }
