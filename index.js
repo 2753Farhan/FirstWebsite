a9f65b3d5a0aa90dddef041c626d894d54856f1f
Md Farhan Islam Shuvro <shuvro.cadet@gmail.com>
2023-06-12 13:31:18 +0600

    modified

--- index.js
+++ index.js
@@ -48,3 +48,60 @@ document.getElementById("rbutton").onclick = function(){
 
     document.getElementById("pr").innerHTML = "Your random number is : " + z;
 }
+
+document.getElementById("cbutton").onclick = function(){
+    if(document.getElementById("pizza").checked){
+        console.log("You selected pizza");
+    }
+    else if(document.getElementById("burger").checked){
+        console.log("You selected burger");
+    }
+    else{
+        console.log("You selected nothing");
+    }
+}
+const ansr=Math.floor(Math.random()*10+1);
+let guesses=0;
+console.log(ansr);
+document.getElementById("guessbutton").onclick = function()
+{
+    let guess=document.getElementById("guessfield").value;
+    guesses+=1;
+    if(guess==ansr){
+        alert(`${ansr} is the number . You took ${guesses} guesses`);
+    }
+
+    else if(guess < ansr){
+        alert('Too small');
+    }
+    else{
+        alert('Too large');
+    }
+    
+}
+
+document.getElementById("tembutton").onclick =function(){
+
+    let temp;
+    temp=document.getElementById("tempval").value;
+    console.log(temp);
+    if(document.getElementById("celciusbutton").checked){
+        temp=Number(temp);
+        temp=tocelcius(temp);
+        document.getElementById("templabel").innerHTML= temp + "degree celcius"; 
+    }
+    else if(document.getElementById("furrenhitebutton").checked){
+        temp=Number(temp);
+        temp=tofarenhite(temp);
+        document.getElementById("templabel").innerHTML= temp + " degree farenhite";
+    }
+    else{
+        document.getElementById("templabel").innerHTML = "Select a unit";
+    }
+}
+function tocelcius(temp){
+    return (temp-32)*(5/9);
+}
+function tofarenhite(temp){
+    return (temp*9/5+32);
+}
